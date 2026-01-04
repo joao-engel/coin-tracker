db4d39ae87e15824b3d3806e9d8811fa806a7b88
Joao Engel <joaovi.engel@gmail.com>
2026-01-04 18:39:16 -0300

    Add endpoint para buscar os ultimos valores das cryptos

---
+++ src/Api.Analytics/Controllers/TickerController.cs
@@ -0,0 +1,32 @@
+﻿using Core.Lib.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Analytics.Controllers;
+
+[ApiController]
+[Route("Api/[controller]")]
+public class TickerController : Controller
+{
+    private readonly ILogger<TickerController> _logger;
+    private readonly PriceCacheService _priceCacheService;
+
+    public TickerController(ILogger<TickerController> logger, PriceCacheService priceCacheService)
+    {
+        _logger = logger;
+        _priceCacheService = priceCacheService;
+    }
+
+    [HttpGet("Latest")]
+    public async Task<IActionResult> GetLatestPrices()
+    {
+        try
+        {
+            return Ok(await _priceCacheService.GetAllPricesAsync());
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Erro ao buscar precos");
+            throw;
+        }
+    }
+}

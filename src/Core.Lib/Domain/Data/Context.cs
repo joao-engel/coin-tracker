9eae7ba77e5454f4c67c2a107259da3842fd2308
Joao Engel <joaovi.engel@gmail.com>
2026-01-20 23:44:19 -0300

    fix: Add conversor de datas no context para tratar tudo como utc

--- src/Core.Lib/Domain/Data/Context.cs
+++ src/Core.Lib/Domain/Data/Context.cs
@@ -1,10 +1,18 @@
 ﻿using Core.Lib.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Core.Lib.Domain.Data;
 public class Context(DbContextOptions<Context> options) : DbContext(options)
 {
     public DbSet<MarketHistory> MarketHistory { get; set; }
+    public DbSet<CryptoAsset> CryptoAssets { get; set; }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<DateTime>()
+            .HaveConversion<UtcValueConverter>();
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -14,5 +22,14 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
         modelBuilder.ApplyConfiguration(new CryptoAssetContext());
 
         base.OnModelCreating(modelBuilder);
+    }
+}
+
+internal class UtcValueConverter : ValueConverter<DateTime, DateTime>
+{
+    public UtcValueConverter() : base(
+        v => v.Kind == DateTimeKind.Utc ? v : DateTime.SpecifyKind(v, DateTimeKind.Utc),
+        v => DateTime.SpecifyKind(v, DateTimeKind.Utc))
+    {
     }
 }
